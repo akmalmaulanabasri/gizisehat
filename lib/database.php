49675aa4bbc877f9e08c79cd60325d1b49ee8280
Akmal Maulana Basri <editorzone.20@gmail.com>
2023-01-25 10:32:20 +0700

    update jk

--- lib/database.php
+++ lib/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "root", "gizisehat");
+$conn = mysqli_connect("localhost", "root", "", "gizisehat");
 
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
